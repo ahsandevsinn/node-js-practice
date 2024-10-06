7fabac7e7766c9a3a1adca25c496804d7926ee52
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2024-10-06 11:04:25 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+// create file using fs.writeFileSync
+// fs.writeFileSync('text.txt','Welcome to text file ');
+// append data into same file
+// fs.appendFileSync("text.txt","Welcome Ahsan into Node Js");
+// read data 
+// const fileData = fs.readFileSync("text.txt");
+// const fileDataString = fileData.toString();
+// console.log(fileDataString);
+// rename file 
+// fs.renameSync("text.txt","readwritetxt");
